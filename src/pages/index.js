7c6c38b640e4805f260f9dc691a47d2c0d95786d
Nick Alexander <nick.alexander@mammothgrowth.com>
2023-01-17 16:08:32 -0500

    changed hero text

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,8 +30,8 @@ const IndexPage = () => {
       <Hero
         maxWidth={'500px'}
         image={'/banner1.png'}
-        title={'Essentials for a cold winter'}
-        subtitle={'Discover Autumn Winter 2021'}
+        title={'Woolys Winter Gear'}
+        subtitle={'Sample site to generate data for Segment Workspace'}
         ctaText={'shop now'}
         ctaAction={goToShop}
       />
